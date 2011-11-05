3976c3e4481e3b923e1f3b7fe62849ed6a02e0a7
Minh <minh.doboi@free.fr>
2011-11-05 22:12:45 +0100

    nothing

--- oc-pawns/PathFinder.ml
+++ oc-pawns/PathFinder.ml
@@ -18,18 +18,18 @@ let stupidFind orig (dx, dy) ({field} as world) =
             let ((x, y) as pn) = ptAdd p step
             and newcost = cost +. stepCost
             in if (x == dx && y == dy)
-              then [(path @ [pn], newcost)]
+              then [(List.rev (pn :: path), newcost)]
               else if (not (isClean world pn) || marks.(x).(y) < newcost)
               then []
               else begin
                 marks.(x).(y) <- newcost;
-                bestDist pn newcost (path @ [pn])
+                bestDist pn newcost (pn :: path)
               end ) stepCosts)
   in bestDist orig 0. []
 
-let pow2 x = x * x
-
-let dumbDist (a, b) (c, d) = pow2 (d - b) + pow2 (c - a)
+let dumbDist (a, b) (c, d) =
+  let pow2 x = x * x
+  in pow2 (d - b) + pow2 (c - a)
 
 let fastStupidFind orig ((dx, dy) as dest) ({field} as world) =
   let rec marks = initMarks field.size
@@ -38,10 +38,10 @@ let fastStupidFind orig ((dx, dy) as dest) ({field} as world) =
       let ((x, y) as pn) = ptAdd p step
       and newcost = cost +. stepCost
       in if (x == dx && y == dy)
-        then ([(path @ [pn], newcost)], r)
+        then ([(List.rev (pn :: path), newcost)], r)
         else if (not (isClean world pn) || marks.(x).(y) < newcost)
         then (l, r)
-        else (l, r @ [(pn, newcost)] )
+        else (l, (pn, newcost) :: r)
     in List.fold_left checkStep ([], []) stepCosts
   and bestDist p cost path =
     let (l, r) = checkSteps p cost path
@@ -49,7 +49,7 @@ let fastStupidFind orig ((dx, dy) as dest) ({field} as world) =
        and getPath acc ((x, y) as pn, newcost) = match acc with
          | [] ->
              marks.(x).(y) <- newcost;
-             bestDist pn newcost (path @ [pn])
+             bestDist pn newcost (pn :: path)
          | l -> l
       in l @ List.fold_left getPath [] rsorted
   in bestDist orig 0. []
