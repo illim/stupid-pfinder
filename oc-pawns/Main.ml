c9c509313a8972ba17735beff3b7f76e03d8d41a
Minh <minh.doboi@free.fr>
2011-11-06 11:22:24 +0100

    module

--- oc-pawns/Main.ml
+++ oc-pawns/Main.ml
@@ -1,5 +1,7 @@
 open Models
-open PathFinder
+
+module SF = PathFinder.Stupidfinder
+module FSF = PathFinder.FastStupidfinder
 
 let basicWorld = {
   field = {
@@ -9,15 +11,6 @@ let basicWorld = {
   entities = List.map (fun i -> {position = (7, i)}) [3; 4; 5; 6; 7; 8]
 }
 
-
-(* some bs :)) *)
 let main =
-  let rec print_pathresult (paths, cost) =
-    List.iter (fun (x, y) -> Printf.printf "(%i," x; Printf.printf " %i)" y) paths;
-    Printf.printf ": %f\n" cost;
-  and print_result r = Printf.printf "%i results " (List.length r);
-    print_pathresult (List.hd (List.sort (fun (_, x) (_, y) -> compare x y) r))
-  and results = (stupidFind (3, 3) (8, 6) basicWorld)
-  and results2 = (fastStupidFind (3, 3) (8, 6) basicWorld)
-  in print_result results;
-    print_result results2
+  SF.printResult(SF.stupidFind (3, 3) (8, 6) basicWorld);
+  FSF.printResult(FSF.stupidFind (3, 3) (8, 6) basicWorld)
--- oc-pawns/PathFinder.ml
+++ oc-pawns/PathFinder.ml
@@ -1,55 +1,81 @@
 open Models
 
-let stepCosts =
-  let ortho = List.map (fun step -> (step, 1.)) [1, 0 ; 0, 1; 0, -1; -1, 0]
-  and diag  = List.map (fun step -> (step, 1.414213)) [1, 1; 1, -1; -1, 1; -1, -1]
-  in ortho @ diag
+module type FINDER = sig
+  type result
+  val stupidFind : point -> point -> world -> result
+  val printResult : result -> unit
+end
 
-let ptAdd (x, y) (i, j) = (x + i, y + j)
+module BaseStupidFinder = struct
+  type result = (point list * float) list
 
-let initMarks size = Array.make_matrix size size 999999.
+  (* some bs :)) *)
+  let printResult result =
+    let print_pathresult (paths, cost) =
+      List.iter (fun (x, y) -> Printf.printf "(%i," x; Printf.printf " %i)" y) paths;
+      Printf.printf ": %f\n" cost
+    in Printf.printf "%i results " (List.length result);
+      print_pathresult (List.hd (List.sort (fun (_, x) (_, y) -> compare x y) result))
 
-let stupidFind orig (dx, dy) ({field} as world) =
-  let rec marks = initMarks field.size
-  and bestDist p cost path =
-    List.flatten
-      (List.map
-         (fun (step, stepCost)->
-            let ((x, y) as pn) = ptAdd p step
-            and newcost = cost +. stepCost
-            in if (x == dx && y == dy)
-              then [(List.rev (pn :: path), newcost)]
-              else if (not (isClean world pn) || marks.(x).(y) < newcost)
-              then []
-              else begin
-                marks.(x).(y) <- newcost;
-                bestDist pn newcost (pn :: path)
-              end ) stepCosts)
-  in bestDist orig 0. []
+  let stepCosts =
+    let ortho = List.map (fun step -> (step, 1.)) [1, 0 ; 0, 1; 0, -1; -1, 0]
+    and diag  = List.map (fun step -> (step, 1.414213)) [1, 1; 1, -1; -1, 1; -1, -1]
+    in ortho @ diag
 
-let dumbDist (a, b) (c, d) =
-  let pow2 x = x * x
-  in pow2 (d - b) + pow2 (c - a)
+  let add (x, y) (i, j) = (x + i, y + j)
 
-let fastStupidFind orig ((dx, dy) as dest) ({field} as world) =
-  let rec marks = initMarks field.size
-  and checkSteps p cost path =
-    let checkStep (l, r) (step, stepCost) =
-      let ((x, y) as pn) = ptAdd p step
-      and newcost = cost +. stepCost
-      in if (x == dx && y == dy)
-        then ([(List.rev (pn :: path), newcost)], r)
-        else if (not (isClean world pn) || marks.(x).(y) < newcost)
-        then (l, r)
-        else (l, (pn, newcost) :: r)
-    in List.fold_left checkStep ([], []) stepCosts
-  and bestDist p cost path =
-    let (l, r) = checkSteps p cost path
-    in let rsorted = List.sort (fun (pn, _) (pn2, _) -> compare (dumbDist pn dest) (dumbDist pn2 dest)) r
-       and getPath acc ((x, y) as pn, newcost) = match acc with
-         | [] ->
-             marks.(x).(y) <- newcost;
-             bestDist pn newcost (pn :: path)
-         | l -> l
+  let initMarks size = Array.make_matrix size size 999999.
+end
+
+module Stupidfinder : FINDER = struct
+  include BaseStupidFinder
+
+  let stupidFind orig (dx, dy) ({field} as world) =
+    let rec marks = initMarks field.size
+    and bestDist p cost path =
+      List.flatten
+        (List.map
+           (fun (step, stepCost)->
+              let ((x, y) as pn) = add p step
+              and newcost = cost +. stepCost
+              in if (x == dx && y == dy)
+                then [(List.rev (pn :: path), newcost)]
+                else if (not (isClean world pn) || marks.(x).(y) < newcost)
+                then []
+                else begin
+                  marks.(x).(y) <- newcost;
+                  bestDist pn newcost (pn :: path)
+                end ) stepCosts)
+    in bestDist orig 0. []
+
+end
+
+module FastStupidfinder : FINDER = struct
+  include BaseStupidFinder
+
+  let dumbDist (a, b) (c, d) = let pow2 x = x * x in pow2 (d - b) + pow2 (c - a)
+
+  let stupidFind orig ((dx, dy) as dest) ({field} as world) =
+    let rec marks = initMarks field.size
+    and checkSteps p cost path =
+      let checkStep (l, r) (step, stepCost) =
+        let ((x, y) as pn) = add p step
+        and newcost = cost +. stepCost
+        in if (x == dx && y == dy)
+          then ([(List.rev (pn :: path), newcost)], r)
+          else if (not (isClean world pn) || marks.(x).(y) < newcost)
+          then (l, r)
+          else (l, (pn, newcost) :: r)
+      in List.fold_left checkStep ([], []) stepCosts
+    and bestDist p cost path =
+      let (l, r) = checkSteps p cost path
+      in let rsorted = List.sort (fun (pn, _) (pn2, _) -> compare (dumbDist pn dest) (dumbDist pn2 dest)) r
+         and getPath acc ((x, y) as pn, newcost) = match acc with
+           | [] ->
+               marks.(x).(y) <- newcost;
+               bestDist pn newcost (pn :: path)
+           | l -> l
       in l @ List.fold_left getPath [] rsorted
-  in bestDist orig 0. []
+    in bestDist orig 0. []
+
+end
